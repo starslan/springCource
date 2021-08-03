ecc37b4b25b79004c7124079ed562a629ddc2c3e
Starslan <starslan-1@yandex.ru>
2021-08-03 17:56:13 +0300

    initial

---
+++ src/test/java/ru/starslan/demo/InstazooApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.starslan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstazooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
