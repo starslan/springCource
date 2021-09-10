e3cb935fb0a52022656f45192a0fd6281e30ed85
Starslan <starslan-1@yandex.ru>
2021-09-10 09:14:23 +0300

    from auth

--- src/client/src/app/auth/register/register.component.ts
+++ src/client/src/app/auth/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../service/auth.service";
+import {TokenStorageService} from "../../service/token-storage.service";
+import {NotificationService} from "../../service/notification.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+ public registerForm: FormGroup;
+
+  constructor(
+      private authService: AuthService,
+      private tokenStorage: TokenStorageService,
+      private notificationService: NotificationService,
+      private router:Router,
+      private fb: FormBuilder
+    ) {
+
+    }
 
   ngOnInit(): void {
+    this.registerForm = this.createRegisterForm();
+  }
+
+  createRegisterForm(): FormGroup{
+    return this.fb.group({
+      username: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.email])],
+      password: ['', Validators.compose([Validators.required])],
+      confirmPassword: ['', Validators.compose([Validators.required])],
+      firstname: ['', Validators.compose([Validators.required])],
+      latsname: ['', Validators.compose([Validators.required])],
+    })
+  }
+
+
+  submit(): void{
+    this.authService.register({
+      username: this.registerForm.value.username,
+      password: this.registerForm.value.password,
+      email: this.registerForm.value.email,
+      firstname: this.registerForm.value.firstname,
+      latsname: this.registerForm.value.latsname,
+    }).subscribe(data=>{
+      console.log(data);
+      this.tokenStorage.saveToken(data.token);
+      this.tokenStorage.saveUser(data);
+      this.notificationService.showSnackBar('Successfully Registered!');
+      this.router.navigate(['/']);
+      window.location.reload();
+    }, error => {
+      console.log(error);
+      this.notificationService.showSnackBar(error.message);
+    })
   }
 
 }
