0f0fa8808ae8797b15a4e8480590ba0e31a05c02
starslan <starslan-1@yandex.ru>
2021-10-05 14:15:33 +0300

    profile update

--- src/client/src/app/user/edit-user/edit-user.component.ts
+++ src/client/src/app/user/edit-user/edit-user.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {NotificationService} from "../../service/notification.service";
+import {UserService} from "../../service/user.service";
+import {User} from "../../models/User";
 
 @Component({
   selector: 'app-edit-user',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  public profileEditForm: FormGroup;
+
+  constructor(
+    private dialogRef: MatDialogRef<EditUserComponent>,
+    private fb: FormBuilder,
+    private notificationService: NotificationService,
+    @Inject(MAT_DIALOG_DATA) public data,
+    private service: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.profileEditForm = this.createProfileForm();
+  }
+
+  createProfileForm(): FormGroup{
+    return this.fb.group({
+      firstName: [this.data.user.firstname, Validators.compose([Validators.required]) ],
+      lastName: [this.data.user.lastname, Validators.compose([Validators.required]) ],
+      bio: [this.data.user.bio, Validators.compose([Validators.required]) ],
+    })
+  }
+
+  submit(): void{
+    this.service.updateUser(this.updateUser())
+      .subscribe(()=>{
+          this.notificationService.showSnackBar('Update user successfully');
+          this.dialogRef.close();
+      })
+  }
+
+  private updateUser(): User{
+    this.data.user.firstname = this.profileEditForm.value.firstname;
+    this.data.user.lastname = this.profileEditForm.value.lastname;
+    this.data.user.bio = this.profileEditForm.value.bio;
+    return this.data.user;
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
   }
 
 }
